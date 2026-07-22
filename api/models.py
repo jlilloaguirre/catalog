872147fa92c4fce75aafb8c820be27083db7ce98
SBA23320 <sba23320@student.cct.ie>
2026-07-22 14:02:29 +0100

    Added a new Book field

--- api/models.py
+++ api/models.py
@@ -6,5 +6,6 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    new_field_1 = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
