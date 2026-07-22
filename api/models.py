bc1461f3c05e323fef2a7122f63502ff16d528ba
SBA23320 <sba23320@student.cct.ie>
2026-07-22 14:15:05 +0100

    Removed test migrations

--- api/models.py
+++ api/models.py
@@ -5,7 +5,5 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     author = models.CharField(max_length=255)
-    new_field_1 = models.CharField(max_length=255, blank=True)
-    new_field_2 = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
