938e86740ca1070be3d1dfaafa780da5f66816d2
SBA23320 <sba23320@student.cct.ie>
2026-08-01 16:23:48 +0100

    fix: serve Django static files correctly in Kubernetes

--- bookcatalog/settings.py
+++ bookcatalog/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,7 +140,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ done
 echo "Database is ready"
 
 python manage.py migrate
+python manage.py collectstatic --noinput
 
-python manage.py runserver 0.0.0.0:8000
+exec gunicorn bookcatalog.wsgi:application --bind 0.0.0.0:8000 --workers 2
